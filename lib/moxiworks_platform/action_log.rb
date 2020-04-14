73da7c30ce3241b6ee930e8600d73f534eab62d5
Tres Wong-Godfrey <tres@wong-godfrey.com>
2020-04-13 17:36:04 -0700

    feat: ActionLog Delete
             \ \ \ \ \ \ \ \ | | | | | | | / / / / / / / /
            \         __    ___    __    __      __       /
           -     v   / _/  /   |  /  |  / _/  /|/ /  v     -
          -     >+<  \_ \ | \/ | | ' | | |_  |   |  >+<     -
           -     ^   /__/ |_||_| |_|_|  \__\ |_|\_\  ^     -
            /                                             \
             / / / / / / / / | | | | | | | \ \ \ \ \ \ \ \

--- lib/moxiworks_platform/action_log.rb
+++ lib/moxiworks_platform/action_log.rb
@@ -1,28 +1,28 @@
 module  MoxiworksPlatform
   class ActionLog < MoxiworksPlatform::Resource
     # @!attribute agent_uuid
-    #   agent_uuid is the Moxi Works Platform ID of the agent which an ActionLog entry is
+    #   agent_uuid is the MoxiWorks Platform ID of the agent which an ActionLog entry is
     #   or is to be associated with. This will be an RFC 4122 compliant UUID.
     #
-    #   this or moxi_works_agent_id must be set for any Moxi Works Platform transaction
+    #   this or moxi_works_agent_id must be set for any MoxiWorks Platform transaction
     #
-    #   @return [String] the Moxi Works Platform ID of the agent
+    #   @return [String] the MoxiWorks Platform ID of the agent
     attr_accessor :agent_uuid
 
     # @!attribute moxi_works_agent_id
-    #   moxi_works_agent_id is the Moxi Works Platform ID of the agent which an ActionLog entry is
+    #   moxi_works_agent_id is the MoxiWorks Platform ID of the agent which an ActionLog entry is
     #   or is to be associated with.
     #
-    #   this or agent_uuid must be set for any Moxi Works Platform transaction
+    #   this or agent_uuid must be set for any MoxiWorks Platform transaction
     #
-    #   @return [String] the Moxi Works Platform ID of the agent
+    #   @return [String] the MoxiWorks Platform ID of the agent
     attr_accessor :moxi_works_agent_id
 
     # @!attribute moxi_works_contact_id
-    #   This is the Moxi Works Platform ID of this Contact that this ActionLog entry is about.
+    #   This is the MoxiWorks Platform ID of this Contact that this ActionLog entry is about.
     #   This will be an RFC 4122 compliant UUID
     #
-    #   this or partner_contact_id must be set for any Moxi Works Platform transaction
+    #   this or partner_contact_id must be set for any MoxiWorks Platform transaction
     #
     #   @return [String] your system's unique ID for the contact
     attr_accessor :moxi_works_contact_id
@@ -30,7 +30,7 @@ module  MoxiworksPlatform
     # @!attribute partner_contact_id
     #   *your system's* unique ID for the Contact
     #
-    #   this or moxi_works_contact_id must be set for any Moxi Works Platform transaction
+    #   this or moxi_works_contact_id must be set for any MoxiWorks Platform transaction
     #
     #   @return [String] your system's unique ID for the contact
     attr_accessor :partner_contact_id
@@ -51,16 +51,16 @@ module  MoxiworksPlatform
     #
     # @return [Array] array containing any ActionLog entries found by search request
     #  {
-    #     moxi_works_action_log_id: [String] unique identifier for the Moxi Works Platform ActionLog entry,
+    #     moxi_works_action_log_id: [String] unique identifier for the MoxiWorks Platform ActionLog entry,
     #     type: [String] the type of ActionLog entry this is. The string should be formatted in lowercase with an underscore between each word,
     #     timestamp: [Integer] Unix timestamp for the creation time of the ActionLog entry,
     #     log_data: [Dictionary] the payload data of the ActionLog entry. The structure returned is dependent on the kind of ActionLog entry this is
     #  }
     attr_accessor :actions
 
-    # Creates a new ActionLog entry in Moxi Works Platform
+    # Creates a new ActionLog entry in MoxiWorks Platform
     # @param [Hash] opts named parameter Hash
-    # @option opts [String]  :moxi_works_agent_id *REQUIRED* The Moxi Works Agent ID for the agent to which this ActionLog entry is to be associated
+    # @option opts [String]  :moxi_works_agent_id *REQUIRED* The MoxiWorks Agent ID for the agent to which this ActionLog entry is to be associated
     # @option opts [String]  :partner_contact_id *REQUIRED* Your system's unique ID for the contact for whom the ActionLog entry is being created.
     # @option opts [String]  :title *REQUIRED*  A brief title for this ActionLog entry (85 characters or less)
     # @option opts [String]  :body *REQUIRED*  The body of this ActionLog entry (255 characters or less)
@@ -79,12 +79,47 @@ module  MoxiworksPlatform
     #     )
     #
     def self.create(opts={})
-       self.send_request(:post, opts)
+      raise ::MoxiworksPlatform::Exception::ArgumentError,
+            'arguments must be passed as named parameters' unless opts.is_a? Hash
+      url ||= "#{MoxiworksPlatform::Config.url}/api/action_logs"
+      required_opts = [:moxi_works_agent_id, :partner_contact_id, :title, :body]
+      required_opts.each do |opt|
+        raise ::MoxiworksPlatform::Exception::ArgumentError, "#{opt} required" if
+            opts[opt].nil? or opts[opt].to_s.empty?
+      end
+       self.send_request(:post, opts, url)
     end
 
-    # Search an Agent's ActionLog entries in Moxi Works Platform
+    # Delete an ActionLog entry your system has previously created in MoxiWorks Platform
     # @param [Hash] opts named parameter Hash
-    # @option opts [String]  :moxi_works_agent_id *REQUIRED* The Moxi Works Agent ID  for the agent to which this ActionLog is associated -- moxi_works_agent_id or agent_uuid must be passed
+    #
+    #     required parameters
+    #
+    # @option opts [String]  :moxi_works_action_log_id *REQUIRED* The MoxiWorks ActionLog ID of the ActionLog entry to be deleted
+    #
+    # @return [MoxiworksPlatform::ActionLog]
+    #
+    # @raise ::MoxiworksPlatform::Exception::ArgumentError if required
+    #     named parameters aren't included
+    #
+    # @example
+    #   success = MoxiWorksPlatform::ActionLog.delete(moxi_works_action_log_id: '123abcd' )
+    #
+    def self.delete(opts={})
+      raise ::MoxiworksPlatform::Exception::ArgumentError,
+            'arguments must be passed as named parameters' unless opts.is_a? Hash
+      url = "#{MoxiworksPlatform::Config.url}/api/action_logs/#{opts[:moxi_works_action_log_id]}"
+      required_opts = [:moxi_works_action_log_id]
+      required_opts.each do |opt|
+        raise ::MoxiworksPlatform::Exception::ArgumentError, "#{opt} required" if
+            opts[opt].nil? or opts[opt].to_s.empty?
+      end
+      self.send_request(:post, opts, url)
+    end
+
+    # Search an Agent's ActionLog entries in MoxiWorks Platform
+    # @param [Hash] opts named parameter Hash
+    # @option opts [String]  :moxi_works_agent_id *REQUIRED* The MoxiWorks Agent ID  for the agent to which this ActionLog is associated -- moxi_works_agent_id or agent_uuid must be passed
     # @option opts [String]  :agent_uuid *REQUIRED* The Agent UUID for the agent to which this ActionLog is associated -- moxi_works_agent_id or agent_uuid must be passed
     # @option opts [String]  :partner_contact_id *REQUIRED* Your system's unique ID for the contact for whom the ActionLog entry is being created. -- partner_contact_id or moxi_works_contact_id must be passed
     # @option opts [String]  :moxi_works_contact_id *REQUIRED* MoxiWorks'  unique ID for the contact for whom the ActionLog entry is being created. -- partner_contact_id or moxi_works_contact_id must be passed
@@ -125,33 +160,6 @@ module  MoxiworksPlatform
         end
       end
       results
-    end
-
-    # Send our remote request to the Moxi Works Platform
-    #
-    # @param [String] method The HTTP method to be used when connecting; ex: :put, :post, :get
-    # @param [Hash] opts
-    # @option opts [String]  :moxi_works_agent_id *REQUIRED* The Moxi Works Agent ID for the agent to which this ActionLog entry is associated
-    # @option opts [String]  :partner_contact_id *REQUIRED* Your system's unique ID for the contact for whom the ActionLog entry is being created.
-    # @option opts [String]  :title *REQUIRED*  A brief title for this ActionLog entry (85 characters or less)
-    # @option opts [String]  :body *REQUIRED*  The body of this ActionLog entry (255 characters or less)
-    #
-    #
-    # @return [MoxiworksPlatform::ActionLog]
-    #
-    # @raise ::MoxiworksPlatform::Exception::ArgumentError if required
-    #     named parameters aren't included
-    #
-    def self.send_request(method, opts={}, url=nil)
-      raise ::MoxiworksPlatform::Exception::ArgumentError,
-            'arguments must be passed as named parameters' unless opts.is_a? Hash
-      url ||= "#{MoxiworksPlatform::Config.url}/api/action_logs"
-      required_opts = [:moxi_works_agent_id, :partner_contact_id, :title, :body]
-      required_opts.each do |opt|
-        raise ::MoxiworksPlatform::Exception::ArgumentError, "#{opt} required" if
-            opts[opt].nil? or opts[opt].to_s.empty?
-      end
-      super(method, opts, url)
     end
   end
 end
